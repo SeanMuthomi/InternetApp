6c30760f89226b799931ddc2acd982ee766213a5
Sean Muthomi <sean.muthomi@strathmore.edu>
2025-09-17 09:19:36 +0300

    update

---
+++ Forms/forms.php
@@ -0,0 +1,29 @@
+<?php
+class Forms{
+    public function signup(){
+    }
+
+    private function submit_button($value, $name){
+?>
+        <button type='submit' class="btn btn-primary" name='<?php echo $name; ?>'><?php echo $value; ?></button>
+<?php
+    }
+
+    public function signin(){
+?>
+
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text"></div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+    <?php $this->submit_button('Sign In', 'signin'); ?> <a href='signup.php'>Don't have an account? Sign Up</a>
+</form>
+<?php
+    }
+}
